37420a5b7b3ed0135787c8f850078d18ed0f0127
Luis Vanegas <lvanegas1429@gmail.com>
2022-02-28 15:48:34 -0600

    SEO implementado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/auth/Login";
-import NuevaCuenta from "./components/auth/NuevaCuenta";
-import Proyectos from "./components/proyectos/Proyectos";
 import ProyectoState from "./context/proyectos/ProyectoState";
 import TareaState from "./context/tareas/tareaState";
 import AlertaState from "./context/alertas/AlertaState";
@@ -9,11 +7,20 @@ import AuthState from "./context/autenticacion/AuthState";
 import tokenAuth from "./config/token";
 import RutaPrivada from "./components/rutas/RutaPrivada";
 
+// Pages
+// import Login from "./components/auth/Login";
+// import NuevaCuenta from "./components/auth/NuevaCuenta";
+// import Proyectos from "./components/proyectos/Proyectos";
+
+// Importacion lazy
+const Login = lazy(() => import("./components/auth/Login"));
+const NuevaCuenta = lazy(() => import("./components/auth/NuevaCuenta"));
+const Proyectos = lazy(() => import("./components/proyectos/Proyectos"));
 // Revisar si tenemos un token
 
-const token = localStorage.getItem('token');
+const token = localStorage.getItem("token");
 
-if(token){
+if (token) {
   tokenAuth(token);
 }
 
@@ -22,15 +29,21 @@ function App() {
     <ProyectoState>
       <TareaState>
         <AlertaState>
-        <AuthState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login}></Route>
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta}></Route>
-              <RutaPrivada exact path="/proyectos" component={Proyectos}/>
-            </Switch>
-          </Router>
-        </AuthState>
+          <AuthState>
+            <Router>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                  <Route exact path="/" component={Login}></Route>
+                  <Route
+                    exact
+                    path="/nueva-cuenta"
+                    component={NuevaCuenta}
+                  ></Route>
+                  <RutaPrivada exact path="/proyectos" component={Proyectos} />
+                </Switch>
+              </Suspense>
+            </Router>
+          </AuthState>
         </AlertaState>
       </TareaState>
     </ProyectoState>
